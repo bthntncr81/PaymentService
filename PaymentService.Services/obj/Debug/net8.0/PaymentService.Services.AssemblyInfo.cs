5ef4c9b8222e4c7583660eea694856cad8602358
bthntncr8181 <batuhanntuncerr@hotmail.com>
2025-01-05 22:01:01 +0300

    chages

--- PaymentService.Services/obj/Debug/net8.0/PaymentService.Services.AssemblyInfo.cs
+++ PaymentService.Services/obj/Debug/net8.0/PaymentService.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentService.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef4b3838cc8f25d27d4b738ad8d6977f1bb510f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentService.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentService.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PaymentService.WebAPI/obj/Debug/net8.0/PaymentService.WebAPI.AssemblyInfo.cs
+++ PaymentService.WebAPI/obj/Debug/net8.0/PaymentService.WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentService.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef4b3838cc8f25d27d4b738ad8d6977f1bb510f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentService.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentService.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
